3f17ea03d092cb399963a71b4fa80e8f32b2bbc8
Frodo Swaggins <samuel.s.degroot@gmail.com>
2025-07-08 08:19:30 -0500

    Update COMPETITION | Camelot.c

--- COMPETITION | Camelot.c
+++ COMPETITION | Camelot.c
@@ -6,8 +6,8 @@ int main()
 //------------------------
 //     WAIT FOR LIGHT
 //------------------------
-//wait_for_light(0);
-//shut_down_in(119);
+wait_for_light(0);
+shut_down_in(119);
     
 enable_servos();
 
@@ -15,27 +15,114 @@ enable_servos();
 //------------------------
 //      GO GET POMS
 //------------------------
-set_servo_position(0, 1853);
+set_servo_position(0, 259);
+set_servo_position(3, 1630);
+still();
+T(1000);
 forward();
 T(2000);
 right();
-T(700);
+T(785);
 forward();
-T(4000);
-set_servo_position(0, 1000);
-backward();
+T(4010);
+set_servo_position(0, 1030);
+still();
 T(1000);
+set_servo_position(3, 1100);
+still();
+T(1000);
+forward();
+T(800);
+backward();
+T(200);
     
 //------------------------
 //      SCORE POMS
 //------------------------
 right();
-T(700);
+T(785);
 forward();
-T(600);
-set_servo_position(0, 1700);
+T(500);
+set_servo_position(0, 259);
+still();
+T(1000);
 backward();
-T(600);
+T(500);
+    
+//------------------------
+//     GET MORE POMS
+//------------------------
+left();
+T(800);
+forward();
+T(200);
+set_servo_position(3, 1630);
+forward();
+T(700);
+set_servo_position(0, 259);
+forward();
+T(3500);
+set_servo_position(0, 1030);
+still();
+T(1000);
+set_servo_position(3, 1100);
+still();
+T(1000);
+backward();
+T(100);
+
+//------------------------
+//    SCORE MORE POMS
+//------------------------
+right();
+T(785);
+forward();
+T(500);
+set_servo_position(0, 500);
+still();
+T(1000);
+backward();
+T(550);
+//------------------------
+//    SILLY THE WATER
+//------------------------    
+left();
+T(800);
+forward();
+T(4700);
+left();
+T(1000);
+set_servo_position(0, 1030);
+set_servo_position(3, 1630);
+still();
+T(1000);
+forward();
+T(1400);
+left();
+T(200);
+right();
+T(400);
+left();
+T(400);
+right();
+T(400);
+left();
+T(400);
+right();
+T(400);
+backward();
+T(900);
+set_servo_position(3,1630);
+forward();
+T(400);
+//EXPERIMENTS
+set_servo_position(0, 1030);
+set_servo_position(3, 400);
+forward();
+T(2000);
+forward();
+T(300);
+
     
 disable_servos();
     
